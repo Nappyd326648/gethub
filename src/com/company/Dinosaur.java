99d15717f13ed120be76dd8df96543792a4a4b29
Eugene stewart <eugenecs326648@yahoo.com>
2015-09-28 13:28:12 -0500

    new

---
+++ src/com/company/Dinosaur.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by Eugene on 9/28/2015.
+ */
+public class Dinosaur {
+
+    public void roar(){
+        System.out.println("GRRR!!!!!");
+    }
+}
---
+++ src/com/company/DinosaurSupervisor.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by Eugene on 9/28/2015.
+ */
+public class DinosaurSupervisor {
+
+    public static void main(String[] args) {
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velociraptor = new Dinosaur();
+        Dinosaur velociraptor2 = new Dinosaur();
+        t_rex.roar();
+        velociraptor.roar();
+        velociraptor2.roar();
+    }
+
+
+}
